1f01e99ed682ad3cee9b622ce0ba71e6f5fca9b1
Andrés Garcés <agarcesb@unal.edu.co>
2024-07-26 13:26:33 -0500

    Cambio de visibilidad de atributos y metodos

--- src/gestorAplicacion/Productos/Producto.java
+++ src/gestorAplicacion/Productos/Producto.java
@@ -4,30 +4,40 @@ import java.io.Serializable;
 
 public class Producto implements Serializable {
     // Atributos de clase
-    private static int id = 0;
+    protected static int id = 0;
 
     // Atributos de instancia
-    private String codigo;
-    private String nombre;
-    private String marca;
-    private String modelo;
-    private String estado;
-    private byte condicion;
-    private float precio;
+    protected byte codigo;
+    protected String nombre;
+    protected String marca;
+    protected String modelo;
+    protected String estado;
+    protected byte condicion;
+    protected float precio;
     protected byte descuento;
     protected int puntosRequeridos;
 
     /* ###Constructores### */
-    public Producto ()
+    public Producto (byte codigo, String nombre, String marca, String modelo, String estado, byte condicion, float precio, byte descuento, int puntosRequeridos) {
+    //this.codigo = codigo;
+      this.nombre = nombre;
+      this.marca = marca;
+      this.modelo = modelo;
+      this.estado = estado;
+      this.condicion = condicion;
+      this.precio = precio;
+      this.descuento = descuento;
+      this.puntosRequeridos = puntosRequeridos;
+    }
 
     // Método para generar el código
-    private String generarCodigo() {
+    protected String generarCodigo() {
         id++;
         return "P" + id;
     }
 
 
-
+    /* ###Setters y Getters### */
     public void setPrecio(float precio) {
         this.precio = precio;
     }
