8d4c9b3ec07363d1bf479b05093989be1f387b0d
andres <sgarces0808@gmail.com>
2024-07-27 21:09:53 -0500

    Cambios a constructor de Juego

--- src/gestorAplicacion/Productos/Juego.java
+++ src/gestorAplicacion/Productos/Juego.java
@@ -9,7 +9,8 @@ public class Juego extends Producto {
 	
 	/* ### Constructores ### */
 	
-	public Juego (String genero, String plataforma) {
+	public Juego (int Codigo, String nombre, int cantidad, int cantidadInicial, boolean prestable, byte condiciones, Fecha fechaLanzamiento, String genero, String plataforma) {
+		super(Codigo, nombre, cantidad, cantidadInicial, prestable, condiciones, fechaLanzamiento);
 		this.genero = genero;
 		this.plataforma = plataforma;
 	}
