0b83f4b9d1151e09825b69ee3d01ff7bfc03b49c
Zusark <149200499+Zusark@users.noreply.github.com>
2024-07-27 19:08:46 -0500

    comienzo de cero

--- src/gestorAplicacion/Productos/Juego.java
+++ src/gestorAplicacion/Productos/Juego.java
@@ -1,50 +1,5 @@
 package gestorAplicacion.Productos;
 
 public class Juego extends Producto {
-    private String genero;
-    private String consola;
-
-    // Constructor principal de la subclase Juego
-    public Juego(String nombre, short año, String genero, String marca, String estado, byte condicion,
-            float precio) {
-        // Llama al constructor de la superclase Producto
-        super(nombre, "N/A", año, "N/A", marca, estado, condicion, precio);
-        this.genero = genero;
-        setCodigo("Juego:" + getCodigo());
-    }
-
-    public Juego(String nombre, short año, String genero, String marca, float precio) {
-        // Llama al constructor con estado y condicion por defecto
-        this(nombre, año, genero, marca, "Stock", (byte) 10, precio);
-    }
-
-    public Juego(String nombre, short año, String genero, float precio) {
-        // Llama al constructor con genero, marca y estado por defecto
-        this(nombre, año, genero, "generica", precio);
-    }
-
-    public Juego(String nombre, String genero, float precio) {
-        // Llama al constructor con año por defecto
-        super(nombre, "N/A", (short) 0, "N/A", "generica", "Stock", (byte) 10, precio);
-        this.genero = genero;
-    }
-
-    public Juego(String nombre, float precio) {
-        // Llama al constructor con año por defecto
-        super(nombre, "N/A", (short) 0, "N/A", "generica", "Stock", (byte) 10, precio);
-        this.genero = "N/A";
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public void setConsola(String consola) {
-        this.consola = consola;
-    }
-
+/*inicio*/
 }
--- src/gestorAplicacion/Productos/Producto.java
+++ src/gestorAplicacion/Productos/Producto.java
@@ -1,52 +1,6 @@
 package gestorAplicacion.Productos;
 
 import java.io.Serializable;
-/*aquí va un comentario */
-public class Producto implements Serializable {
-    // Atributos de clase
-    protected static int id = 0;
-
-    // Atributos de instancia
-    protected byte codigo;
-    protected String nombre;
-    protected String marca;
-    protected String modelo;
-    protected String estado;
-    protected byte condicion;
-    protected float precio;
-    protected byte descuento;
-    protected int puntosRequeridos;
-
-    /* ###Constructores### */
-    public Producto (byte codigo, String nombre, String marca, String modelo, String estado, byte condicion, float precio, byte descuento, int puntosRequeridos) {
-    //this.codigo = codigo;
-      this.nombre = nombre;
-      this.marca = marca;
-      this.modelo = modelo;
-      this.estado = estado;
-      this.condicion = condicion;
-      this.precio = precio;
-      this.descuento = descuento;
-      this.puntosRequeridos = puntosRequeridos;
-    }
-
-    // Método para generar el código
-    protected String generarCodigo() {
-        id++;
-        return "P" + id;
-    }
-
-
-    /* ###Setters y Getters### */
-    public void setPrecio(float precio) {
-        this.precio = precio;
-    }
-
-    public float getPrecio() {
-        return precio;
-    }
-
-    public String toString() {
-	    //pendiente
-    }
+public class Producto{
+   /*Inicio*/
 }
