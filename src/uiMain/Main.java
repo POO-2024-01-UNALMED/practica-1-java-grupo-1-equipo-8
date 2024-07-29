c88d98c05b14ea42c8f02d902496a223b08f0a11
Andrés Garcés <agarcesb@unal.edu.co>
2024-07-29 18:37:42 -0500

    Clase Main añadida

---
+++ src/uiMain/Main.java
@@ -0,0 +1,17 @@
+package uiMain;
+
+import gestorAplicacion.Productos.Accesorio;
+
+public class Main {
+    public static void main(String[] args) {
+        // Pruebas para el separador de la TUI
+        /*
+        String separador = "▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒"; //▅
+        System.out.println(separador);
+        */
+
+
+
+        
+    }
+}
