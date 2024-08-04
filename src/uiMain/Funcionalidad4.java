2a9a2de7ce70768c1dc18842679a9c3516c7e8d0
dvillaal <dvillaal@unal.edu.co>
2024-08-04 18:14:24 -0500

    funcion identificar empleado

--- src/uiMain/Funcionalidad4.java
+++ src/uiMain/Funcionalidad4.java
@@ -1,15 +1,65 @@
 package uiMain;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
+import static uiMain.Main.imprimirSeparador;
+
+import gestorAplicacion.manejoLocal.Tienda;
+import gestorAplicacion.personas.Empleado;
 
 public class Funcionalidad4 {
     static Scanner sc = new Scanner(System.in);
-    public static void inspeccionEmpleado(){
-
+    public static void inspeccionEmpleado(Tienda local){
+        identificarEmpleado(local);
     }
 
-    private static void mofificarMetas(){
+    private static Empleado identificarEmpleado(Tienda local){
+        imprimirSeparador();
+
+        Empleado empleado = null;
+        int cedula = 0;
+
+        //Mostrar empleados de la tienda
         System.out.println("Empleados de la tienda:");
 
+        for (Empleado e : local.getEmpleados()) {
+            System.out.println("* " + e.getNombre() + " - " + e.getCedula());
+        }
+
+        while (empleado == null) {
+            System.out.println("Ingrese la cédula del empleado");
+
+            // Buscar al empleado en la lista de empleados por su cédula
+            try {
+                cedula = sc.nextInt();
+
+                for (Empleado e : local.empleados){
+                    if (e.getCedula() == cedula){
+                        empleado = e;
+                        return empleado;
+                    }
+                }
+            } catch (InputMismatchException error) {
+                System.out.println("\n### ERROR ###");
+                System.out.println("Ingrese un número válido. Presiona enter para volver a intentar.\n");
+                sc.nextLine();  // nextLine para limpiar el buffer
+                sc.nextLine();  // nextLine para esperar a que el usuario presione Enter
+                continue;
+            }
+
+            // En caso de que el cliente no sea encontrado dar la opción de intentar de nuevo
+            if (cedula == 0) {
+                System.out.println("\n### ERROR ###");
+                System.out.println("Cliente no encontrado. ¿Desea intentar de nuevo? (Y/n).\n");
+                char decision = 'y';
+                decision = sc.next().charAt(0);
+                if (decision == 'n' || decision == 'N') {
+                    return null;
+                }
+            }
+        }
+
+        return empleado;
     }
 }
