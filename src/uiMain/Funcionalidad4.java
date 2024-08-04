33d45675f9c03c9a4af6cf5dcd3b4f2762f75ad0
andres <sgarces0808@gmail.com>
2024-08-04 15:57:34 -0500

    Refactorizado nombre de clase de funcionalidad

--- src/uiMain/Funcionalidad4.java
+++ src/uiMain/Funcionalidad4.java
@@ -1,12 +1,8 @@
 package uiMain;
 
-import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
-import static uiMain.Main.imprimirSeparador;
-
-public class Funcionalidad2 {
+public class Funcionalidad4 {
     static Scanner sc = new Scanner(System.in);
     public static void inspeccionEmpleado(){
 
